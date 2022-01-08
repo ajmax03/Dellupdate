d4516df85ed38ca1de4c9196c7a3cb59a62c68dc
ajmax03 <96718307+ajmax03@users.noreply.github.com>
2022-01-08 10:21:21 +0000

    Update Dell_update.bat

--- Dell_update.bat
+++ Dell_update.bat
@@ -1,5 +1,5 @@
 @echo off
 
-"C:\Program Files (x86)\Dell\CommandUpdate\dcu-cli.exe" /scan -silent -outputLog=C:\Dell-CU-scan.log
+"C:\Program Files (x86)\Dell\CommandUpdate\dcu-cli.exe" /scan -silent -outputLog=C:\Users\%username%\Metapraxis Ltd\IT Logs - Documents\%hostname%.txt\Dell-CU-scan.log
 
-"C:\Program Files (x86)\Dell\CommandUpdate\dcu-cli.exe" /applyUpdates -silent -reboot=enable -outputLog=C:\Dell-CU-apply.log
+"C:\Program Files (x86)\Dell\CommandUpdate\dcu-cli.exe" /applyUpdates -silent -reboot=enable -outputLog=C:\Users\%username%\Metapraxis Ltd\IT Logs - Documents\%hostname%.txt\Dell-CU-apply.log
